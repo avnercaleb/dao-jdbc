de41be60daf51c481144b0462dd2cda4aadec8d1
avnercaleb <avnercaleb@hotmail.com>
2022-11-01 22:32:00 -0300

    Criação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department dp = new Department(1, "Books");
+		Seller seller = new Seller(21, "Avner", "avnercaleb@email.com", new Date(), 2000.00, dp);
 		
+		System.out.println(seller);
 		System.out.println(dp);
 	}
 
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,18 +1,61 @@
 package model.entities;
 
-import java.time.LocalDate;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Seller {
+public class Seller implements Serializable{	
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String name;
 	private String email;
-	private LocalDate bithDate;
+	private Date bithDate;
 	private Double baseSalary;
+	
+	private Department department;
+	
+	public Seller(){
+		
+	}
+
+	public Seller(Integer id, String name, String email, Date bithDate, Double baseSalary, Department department) {		
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.bithDate = bithDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", bithDate=" + bithDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+	
+	
 }
